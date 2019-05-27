320d60324b9961c177871334b24d6fff02dc3eb1
lawlor <shep34@msn.com>
2019-05-27 21:51:22 +0100

    created skeleton of spec.index

--- spec/index.js
+++ spec/index.js
@@ -6,3 +6,31 @@ const app = require("../app");
 const connection = require("../db/connection");
 
 const request = supertest(app);
+
+describe("/graphql", () => {
+  beforeEach(() =>
+    connection.migrate
+      .rollback()
+      .then(() => connection.migrate.latest())
+      .then(() => connection.seed.run())
+  );
+  after(() => connection.destroy());
+
+  describe("queries", () => {
+    describe("device_types", () => {});
+    describe("brands", () => {});
+    describe("operating_systems", () => {});
+    describe("departments", () => {});
+    describe("employees", () => {});
+    describe("devices", () => {});
+  });
+
+  describe("mutations", () => {
+    describe("device_types", () => {});
+    describe("brands", () => {});
+    describe("operating_systems", () => {});
+    describe("departments", () => {});
+    describe("employees", () => {});
+    describe("devices", () => {});
+  });
+});
