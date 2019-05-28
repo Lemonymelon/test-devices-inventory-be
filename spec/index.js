0e76319275dcb6754a901ae8e2d354a4c77b662d
lawlor <shep34@msn.com>
2019-05-28 12:49:17 +0100

    first test passing

--- spec/index.js
+++ spec/index.js
@@ -19,13 +19,14 @@ describe("/graphql", () => {
   describe("queries", () => {
     describe("device_types", () => {
       it.only("returns all device types when no arguments are passed", () => {
-        const query = "{ deviceType { type_id\ntype_name } }";
+        const query = "{ deviceType { type_id\ntype_id\ntype_name } }";
         return request
           .get("/graphql")
           .send({ query })
           .then(({ body: { data: { deviceType } } }) => {
             expect(deviceType.length).to.equal(3);
             expect(deviceType[0].type_id).to.equal("1");
+            expect(Object.keys(deviceType[0])).to.eql(["type_id", "type_name"]);
           });
       });
     });
